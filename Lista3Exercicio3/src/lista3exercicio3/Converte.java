16dd8899ef537f54e16e9ef3e921ad131fad8335
alexandreps1123 <alexandre.ps1123@gmail.com>
2021-09-02 14:04:38 -0300

    Conversao de temperatura celsius e fahrenheit

---
+++ Lista3Exercicio3/src/lista3exercicio3/Converte.java
@@ -0,0 +1,24 @@
+package lista3exercicio3;
+
+import java.util.Scanner;
+
+public class Converte {
+
+    public static void main(String[] args) {
+        
+        Scanner teclado = new Scanner(System.in);
+        
+        double entradaTemperatura = teclado.nextDouble();
+        teclado.nextLine();
+        
+        Temperatura temperatura = new Temperatura(entradaTemperatura);
+        
+        temperatura.converterCelsius2Fahrenheit(entradaTemperatura);
+        System.out.println("temperatura: " + temperatura.toString() +" graus fahrenheit");
+        
+        temperatura.converterFahrenheit2Celsius(entradaTemperatura);
+        System.out.println("temperatura: " + temperatura.toString() + " graus celsius");
+        
+    }
+    
+}
