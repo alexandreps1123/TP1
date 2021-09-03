6565e23e8a9ab6f22dc4cdd39b5e6ec01bb15f2f
alexandreps1123 <alexandre.ps1123@gmail.com>
2021-09-03 02:14:41 -0300

    objetivo: inicilização de atributo usando construtor, alterar valor por meio de metodos da clase, usar o metodo toString

---
+++ L3E3ConversaoDeTemperaturas/src/l3e3conversaodetemperaturas/Temperatura.java
@@ -0,0 +1,35 @@
+package l3e3conversaodetemperaturas;
+
+public class Temperatura 
+{
+    //atributo
+    private double temperatura;
+    
+    //metodo construtor
+    public Temperatura(double temperatura)
+    {
+        this.temperatura = temperatura;
+    }
+    
+    public void converterCelsius2Fahrenheit(double temperatura){
+        if (temperatura != this.temperatura)
+            this.temperatura = temperatura;
+        else
+            this.temperatura = temperatura * 9.0 / 5.0 + 32;
+    }
+    
+    public void converterFahrenheit2Celsius(double temperatura){
+        if (temperatura != this.temperatura)
+            this.temperatura = temperatura;
+        else
+            this.temperatura = (temperatura - 32.0) * 5.0 / 9.0;
+    }
+    
+    @Override
+    public String toString(){
+       String temperatura = "";
+       temperatura = temperatura + this.temperatura;
+       return temperatura; 
+    }
+    
+}
